d41d90d38f72450580260360535bf571d8e348de
vivian.yu <vivian.yu@dianping.com>
2016-01-06 18:42:09 +0800

    add pom

--- src/main/java/com/github/hadoop/WordCount.java
+++ src/main/java/com/github/hadoop/WordCount.java
@@ -1,7 +1,11 @@
 package com.github.hadoop;
 
+import org.apache.hadoop.io.IntWritable;
+
 /**
  * Created by vivian on 16/1/4.
  */
 public class WordCount {
+
+    public final static IntWritable one = new IntWritable(1);
 }
