4779fbed83da06358918e3514187ca293d9a5ba7
vivian.yu <vivian.yu@dianping.com>
2016-01-12 10:45:50 +0800

    hadoop test

--- src/main/java/WordCount.java
+++ src/main/java/WordCount.java
@@ -73,14 +73,6 @@ public class WordCount {
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
-
-
-//        JobConf conf = new JobConf(WordCount.class);
-//        conf.setJobName("wordcount");
-//        conf.setOutputKeyClass(Text.class);
-//        conf.setOutputValueClass(IntWritable.class);
-//        conf.setMapperClass(Map.class);
-//        conf
     }
 
 
